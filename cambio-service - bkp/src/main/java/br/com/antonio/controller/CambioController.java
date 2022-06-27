c4848574424fbf2dbf51fd7c3050a2b5c59ffbf7
Antonio06Martins <toni6martins@gmail.com>
2022-06-26 21:06:41 -0300

    micro services and tracing

--- cambio-service - bkp/src/main/java/br/com/antonio/controller/CambioController.java
+++ cambio-service - bkp/src/main/java/br/com/antonio/controller/CambioController.java
@@ -5,6 +5,10 @@ import java.math.RoundingMode;
 
 import br.com.antonio.model.Cambio;
 import br.com.antonio.repository.CambioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +16,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Cambio Service API")
 @RestController
-@RequestMapping("/cambio-service")
+@RequestMapping("cambio-service")
 public class CambioController {
+
+    private Logger logger = LoggerFactory.getLogger(CambioController.class);
 
     @Autowired
     private Environment environment;
@@ -22,6 +29,7 @@ public class CambioController {
     @Autowired
     private CambioRepository repository;
 
+    @Operation(description = "Get cambio from currency!")
     @GetMapping(value = "/{amount}/{from}/{to}")
     public Cambio getCambio(
             @PathVariable("amount") BigDecimal amount,
@@ -29,6 +37,7 @@ public class CambioController {
             @PathVariable("to") String to
     ) {
 
+        logger.info("getCambio is called with -> {}, {} and {}", amount, from, to);
         var cambio = repository.findByFromAndTo(from, to);
         if (cambio == null) throw new RuntimeException("Currency Unsupported");
 
